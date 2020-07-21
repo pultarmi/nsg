4a5bce0aa20915e4d3743e9e144021ccd0b36c41
pultarmi <milan.pultar@gmail.com>
2020-07-21 20:43:10 +0200

    fast push

--- tests/test_nsg_optimized_search.cpp
+++ tests/test_nsg_optimized_search.cpp
@@ -88,7 +88,7 @@ int main(int argc, char **argv) {
 //        int a;
 //        std::cin >> a;
 //        std::cout << a + 10 << std::endl;
-        std::cout << 10 << 20 << std::endl;
+//        std::cout << 10 << 20 << std::endl;
     }
 
 //    auto s = std::chrono::high_resolution_clock::now();
