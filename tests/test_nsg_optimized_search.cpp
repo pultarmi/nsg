2bc86db84c0cf4fb1e3bc13a30474ccf96ab6dbf
pultarmi <milan.pultar@gmail.com>
2020-07-21 15:23:56 +0200

    fast push

--- tests/test_nsg_optimized_search.cpp
+++ tests/test_nsg_optimized_search.cpp
@@ -51,12 +51,12 @@ int main(int argc, char **argv) {
     unsigned points_num, dim;
     load_data(argv[1], data_load, points_num, dim);
     float *query_load = NULL;
-    unsigned query_num, query_dim;
-    load_data(argv[2], query_load, query_num, query_dim);
-    assert(dim == query_dim);
+//    unsigned query_num, query_dim;
+//    load_data(argv[2], query_load, query_num, query_dim);
+//    assert(dim == query_dim);
 
-    unsigned L = (unsigned) atoi(argv[4]);
-    unsigned K = (unsigned) atoi(argv[5]);
+    unsigned L = (unsigned) atoi(argv[3]);
+    unsigned K = (unsigned) atoi(argv[4]);
 
     if (L < K) {
         std::cout << "search_L cannot be smaller than search_K!" << std::endl;
@@ -67,17 +67,20 @@ int main(int argc, char **argv) {
     // align the data before build query_load = efanna2e::data_align(query_load,
     // query_num, query_dim);
     efanna2e::IndexNSG index(dim, points_num, efanna2e::FAST_L2, nullptr);
-    index.Load(argv[3]);
+    index.Load(argv[2]);
     index.OptimizeGraph(data_load);
 
     efanna2e::Parameters paras;
     paras.Set<unsigned>("L_search", L);
     paras.Set<unsigned>("P_search", L);
 
+    std::cout << "STARTING QUERY\n";
+
+    unsigned query_num, query_dim;
+    load_data(argv[2], query_load, query_num, query_dim);
+    assert(dim == query_dim);
     std::vector<std::vector<unsigned> > res(query_num);
     for (unsigned i = 0; i < query_num; i++) res[i].resize(K);
-
-    std::cout << "STARTING QUERY\n";
 
     while (1) {
         int a;
