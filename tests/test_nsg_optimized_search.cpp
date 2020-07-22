53a24005e416386e48f6aa492a705e91724e1fb8
pultarmi <milan.pultar@gmail.com>
2020-07-22 09:59:51 +0200

    fast push

--- tests/test_nsg_optimized_search.cpp
+++ tests/test_nsg_optimized_search.cpp
@@ -7,26 +7,8 @@
 #include <chrono>
 #include <string>
 
-void load_data(char *filename, float *&data, unsigned &num, unsigned &dim) {  // load data with sift10K pattern
-    std::ifstream in(filename, std::ios::binary);
-    if (!in.is_open()) {
-        std::cout << "open file error" << std::endl;
-        exit(-1);
-    }
-    in.read((char *) &dim, 4);
-    // std::cout<<"data dimension: "<<dim<<std::endl;
-    in.seekg(0, std::ios::end);
-    std::ios::pos_type ss = in.tellg();
-    size_t fsize = (size_t) ss;
-    num = (unsigned) (fsize / (dim + 1) / 4);
-    data = new float[(size_t) num * (size_t) dim];
-
-    in.seekg(0, std::ios::beg);
-    for (size_t i = 0; i < num; i++) {
-        in.seekg(4, std::ios::cur);
-        in.read((char *) (data + i * dim), dim * 4);
-    }
-    in.close();
+void load_data(float *&data, unsigned dim) {  // load data with sift10K pattern
+    std::cin.read((char *) data, dim * 4);
 }
 
 void save_result(const char *filename, std::vector<std::vector<unsigned> > &results) {
@@ -43,53 +25,42 @@ void save_result(const char *filename, std::vector<std::vector<unsigned> > &resu
 int main(int argc, char **argv) {
     if (argc != 6) {
         std::cout << argv[0]
-                  << " data_file nsg_path search_L search_K result_path"
+                  << " data_file query_dim nsg_path search_L search_K result_path"
                   << std::endl;
         exit(-1);
     }
     float *data_load = NULL;
     unsigned points_num, dim;
-    load_data(argv[1], data_load, points_num, dim);
+    auto query_dim = (unsigned) atoi(argv[2]);
     float *query_load = NULL;
-//    unsigned query_num, query_dim;
-//    load_data(argv[2], query_load, query_num, query_dim);
-//    assert(dim == query_dim);
 
-    unsigned L = (unsigned) atoi(argv[3]);
-    unsigned K = (unsigned) atoi(argv[4]);
+    unsigned L = (unsigned) atoi(argv[4]);
+    unsigned K = (unsigned) atoi(argv[5]);
 
     if (L < K) {
         std::cout << "search_L cannot be smaller than search_K!" << std::endl;
         exit(-1);
     }
 
-    // data_load = efanna2e::data_align(data_load, points_num, dim);//one must
-    // align the data before build query_load = efanna2e::data_align(query_load,
-    // query_num, query_dim);
     efanna2e::IndexNSG index(dim, points_num, efanna2e::FAST_L2, nullptr);
-    index.Load(argv[2]);
+    index.Load(argv[3]);
     index.OptimizeGraph(data_load);
 
     efanna2e::Parameters paras;
     paras.Set<unsigned>("L_search", L);
     paras.Set<unsigned>("P_search", L);
 
-    std::cout << "STARTING QUERY\n";
-
-//    unsigned query_num, query_dim;
-//    load_data(argv[2], query_load, query_num, query_dim);
-//    assert(dim == query_dim);
-//    std::vector<std::vector<unsigned> > res(query_num);
-//    for (unsigned i = 0; i < query_num; i++) res[i].resize(K);
-
-    std::cout << 10 << 20 << std::endl;
-    std::cout.flush();
+    query_load = new float[(size_t) (size_t) dim];
 
     while (1) {
-//        int a;
-//        std::cin >> a;
-//        std::cout << a + 10 << std::endl;
-//        std::cout << 10 << 20 << std::endl;
+        load_data(query_load, query_dim);
+        assert(dim == query_dim);
+        std::vector<std::vector<unsigned> > res(query_num);
+        for (unsigned i = 0; i < query_num; i++) res[i].resize(K);
+
+        for (unsigned i = 0; i < query_num; i++) {
+            index.SearchWithOptGraph(query_load + i * dim, K, paras, res[i].data());
+        }
     }
 
 //    auto s = std::chrono::high_resolution_clock::now();
