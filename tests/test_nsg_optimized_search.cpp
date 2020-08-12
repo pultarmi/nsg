e60413af2028cc46c6325fd5d342c0a05e2f2b45
pultarmi <milan.pultar@gmail.com>
2020-08-12 15:08:26 +0200

    continuous query; refactor

--- tests/test_nsg_optimized_search.cpp
+++ tests/test_nsg_optimized_search.cpp
@@ -7,86 +7,104 @@
 #include <chrono>
 #include <string>
 
-void load_data(char* filename, float*& data, unsigned& num,
-               unsigned& dim) {  // load data with sift10K pattern
-  std::ifstream in(filename, std::ios::binary);
-  if (!in.is_open()) {
-    std::cout << "open file error" << std::endl;
-    exit(-1);
-  }
-  in.read((char*)&dim, 4);
-  // std::cout<<"data dimension: "<<dim<<std::endl;
-  in.seekg(0, std::ios::end);
-  std::ios::pos_type ss = in.tellg();
-  size_t fsize = (size_t)ss;
-  num = (unsigned)(fsize / (dim + 1) / 4);
-  data = new float[(size_t)num * (size_t)dim];
+void load_data(char* filename, float*& data, unsigned& num, unsigned& dim) {  // load data with sift10K pattern
+    std::ifstream in(filename, std::ios::binary);
+    if (!in.is_open()) {
+        std::cout << "open file error" << std::endl;
+        exit(-1);
+    }
+    in.read((char*)&dim, 4);
+    // std::cout<<"data dimension: "<<dim<<std::endl;
+    in.seekg(0, std::ios::end);
+    std::ios::pos_type ss = in.tellg();
+    size_t fsize = (size_t)ss;
+    num = (unsigned)(fsize / (dim + 1) / 4);
+    data = new float[(size_t)num * (size_t)dim];
 
-  in.seekg(0, std::ios::beg);
-  for (size_t i = 0; i < num; i++) {
-    in.seekg(4, std::ios::cur);
-    in.read((char*)(data + i * dim), dim * 4);
-  }
-  in.close();
+    in.seekg(0, std::ios::beg);
+    for (size_t i = 0; i < num; i++) {
+        in.seekg(4, std::ios::cur);
+        in.read((char*)(data + i * dim), dim * 4);
+    }
+    in.close();
 }
 
-void save_result(const char* filename,
-                 std::vector<std::vector<unsigned> >& results) {
-  std::ofstream out(filename, std::ios::binary | std::ios::out);
-
-  for (unsigned i = 0; i < results.size(); i++) {
-    unsigned GK = (unsigned)results[i].size();
-    out.write((char*)&GK, sizeof(unsigned));
-    out.write((char*)results[i].data(), GK * sizeof(unsigned));
-  }
-  out.close();
+void load_query(float *&data, unsigned dim) {  // load data with sift10K pattern
+    std::cin.read((char *) data, dim * 4);
 }
-int main(int argc, char** argv) {
-  if (argc != 7) {
-    std::cout << argv[0]
-              << " data_file query_file nsg_path search_L search_K result_path"
-              << std::endl;
-    exit(-1);
-  }
-  float* data_load = NULL;
-  unsigned points_num, dim;
-  load_data(argv[1], data_load, points_num, dim);
-  float* query_load = NULL;
-  unsigned query_num, query_dim;
-  load_data(argv[2], query_load, query_num, query_dim);
-  assert(dim == query_dim);
 
-  unsigned L = (unsigned)atoi(argv[4]);
-  unsigned K = (unsigned)atoi(argv[5]);
+void save_result(const char *filename, std::vector<std::vector<unsigned> > &results) {
+    std::ofstream out(filename, std::ios::binary | std::ios::out);
 
-  if (L < K) {
-    std::cout << "search_L cannot be smaller than search_K!" << std::endl;
-    exit(-1);
-  }
+    for (unsigned i = 0; i < results.size(); i++) {
+        unsigned GK = (unsigned) results[i].size();
+        out.write((char *) &GK, sizeof(unsigned));
+        out.write((char *) results[i].data(), GK * sizeof(unsigned));
+    }
+    out.close();
+}
 
-  // data_load = efanna2e::data_align(data_load, points_num, dim);//one must
-  // align the data before build query_load = efanna2e::data_align(query_load,
-  // query_num, query_dim);
-  efanna2e::IndexNSG index(dim, points_num, efanna2e::FAST_L2, nullptr);
-  index.Load(argv[3]);
-  index.OptimizeGraph(data_load);
+void write_result( std::vector<unsigned> &results) {
+    unsigned GK = (unsigned) results.size();
+//    std::cout.write((char *) &GK, sizeof(unsigned));
+    std::cout.write((char *) results.data(), GK * sizeof(unsigned));
+    std::cout.flush();
+}
 
-  efanna2e::Parameters paras;
-  paras.Set<unsigned>("L_search", L);
-  paras.Set<unsigned>("P_search", L);
+int main(int argc, char **argv) {
+    if (argc != 6) {
+        std::cout << argv[0]
+                  << " data_file query_dim nsg_path search_L search_K"
+                  << std::endl;
+        exit(-1);
+    }
+    float *data_load = NULL;
+    unsigned points_num, dim;
+    load_data(argv[1], data_load, points_num, dim);
+    auto query_dim = (unsigned) atoi(argv[2]);
+    float *query_load = NULL;
 
-  std::vector<std::vector<unsigned> > res(query_num);
-  for (unsigned i = 0; i < query_num; i++) res[i].resize(K);
+    unsigned L = (unsigned) atoi(argv[4]);
+    unsigned K = (unsigned) atoi(argv[5]);
 
-  auto s = std::chrono::high_resolution_clock::now();
-  for (unsigned i = 0; i < query_num; i++) {
-    index.SearchWithOptGraph(query_load + i * dim, K, paras, res[i].data());
-  }
-  auto e = std::chrono::high_resolution_clock::now();
-  std::chrono::duration<double> diff = e - s;
-  std::cout << "search time: " << diff.count() << "\n";
+    if (L < K) {
+        std::cout << "search_L cannot be smaller than search_K!" << std::endl;
+        exit(-1);
+    }
 
-  save_result(argv[6], res);
+    efanna2e::IndexNSG index(dim, points_num, efanna2e::FAST_L2, nullptr);
+    index.Load(argv[3]);
+    index.OptimizeGraph(data_load);
+    assert(dim == query_dim);
+    delete[] data_load;
 
-  return 0;
+//    std::cout << "aaa" << std::endl;
+//    float* aux = new float[1000000000];
+//    std::cout << aux[0];
+//    std::
+
+    efanna2e::Parameters paras;
+    paras.Set<unsigned>("L_search", L);
+    paras.Set<unsigned>("P_search", L);
+
+    query_load = new float[(size_t) dim];
+    std::vector<unsigned> res(K);
+
+    while (!std::cin.eof()) {
+        load_query(query_load, query_dim);
+        index.SearchWithOptGraph(query_load, K, paras, res.data());
+        write_result(res);
+    }
+
+//    auto s = std::chrono::high_resolution_clock::now();
+//    for (unsigned i = 0; i < query_num; i++) {
+//        index.SearchWithOptGraph(query_load + i * dim, K, paras, res[i].data());
+//    }
+//    auto e = std::chrono::high_resolution_clock::now();
+//    std::chrono::duration<double> diff = e - s;
+//    std::cout << "search time: " << diff.count() << "\n";
+//
+//    save_result(argv[6], res);
+
+    return 0;
 }
