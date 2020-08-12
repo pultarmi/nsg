8179ce14a01217f970aea463fb6e42f38eb56f21
pultarmi <milan.pultar@gmail.com>
2020-08-12 14:41:41 +0200

    fast push

--- tests/test_nsg_optimized_search.cpp
+++ tests/test_nsg_optimized_search.cpp
@@ -19,7 +19,7 @@ void load_data(char* filename, float*& data, unsigned& num, unsigned& dim) {  //
     std::ios::pos_type ss = in.tellg();
     size_t fsize = (size_t)ss;
     num = (unsigned)(fsize / (dim + 1) / 4);
-    data = new float[(size_t)num * (size_t)dim * 100];
+    data = new float[(size_t)num * (size_t)dim];
 
     in.seekg(0, std::ios::beg);
     for (size_t i = 0; i < num; i++) {
@@ -76,7 +76,10 @@ int main(int argc, char **argv) {
     index.Load(argv[3]);
     index.OptimizeGraph(data_load);
     assert(dim == query_dim);
-//    delete[] data_load;
+    delete[] data_load;
+
+    std::cout << "aaa" << std::endl;
+    float* aux = new float[1000000];
 
     efanna2e::Parameters paras;
     paras.Set<unsigned>("L_search", L);
