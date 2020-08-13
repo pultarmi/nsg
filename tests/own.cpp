cec6756b64d291f230bcd36036cfd2a09e997b16
pultarmi <milan.pultar@gmail.com>
2020-08-13 14:50:57 +0200

    fast push

--- tests/own.cpp
+++ tests/own.cpp
@@ -4,11 +4,11 @@
 
 int main(int argc, char **argv) {
     efanna2e::IndexNSG index(0, 0, efanna2e::FAST_L2, nullptr);
-    float* aux = new float[1000000000];
-    for(int i=0; i < 1000000000; i++){
-        aux[i] = 1;
-    }
-    usleep(10000000);
+//    float* aux = new float[1000000000];
+//    for(int i=0; i < 1000000000; i++){
+//        aux[i] = 1;
+//    }
+//    usleep(10000000);
 
     std::cout << "Hello, World!" << std::endl;
     return 0;
