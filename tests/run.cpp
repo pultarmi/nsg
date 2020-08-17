798df64529956b239fa7c6c3191112627117f3bb
pultarmi <milan.pultar@gmail.com>
2020-08-17 13:17:00 +0200

    fast push

--- tests/run.cpp
+++ tests/run.cpp
@@ -178,10 +178,10 @@ int main(int argc, char **argv) {
         auto aux = futures[i].get();
         for(unsigned j=0; j<aux.first.size(); j++)
             aux.first[j] += offset;
-        std::cout << aux.first[0] << std::endl;
-        std::cout << indices_[0] << std::endl;
         indices_.insert(indices_.end(), aux.first.begin(), aux.first.end());
         dists_.insert(dists_.end(), aux.second.begin(), aux.second.end());
+        std::cout << aux.first[0] << std::endl;
+        std::cout << indices_[0] << std::endl;
         offset += searchers[i].points_num;
     }
 
