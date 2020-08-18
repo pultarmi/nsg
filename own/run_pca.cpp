abfc0cbe5e99f070e6e5c78a52da745d911f6e1e
pultarmi <milan.pultar@gmail.com>
2020-08-18 16:26:40 +0200

    fast push

--- own/run_pca.cpp
+++ own/run_pca.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "run_pca.h"
+#include "utils.h"
 #include <efanna2e/index_nsg.h>
 #include <efanna2e/util.h>
 #include <iostream>
@@ -18,10 +19,14 @@ namespace fs = std::experimental::filesystem;
 // path='/home/mpultar/Data/mix_fc:plus_pca:pca-P5-36_splits:36_1-1--1_nsg'
 // ~/nsg/build/own/run_pca /home/mpultar/Data/pca-P5-36
 int main(int argc, char **argv) {
-    if (argc != 2) {
-        std::cout << argv[0] << " path_pca" << std::endl;
+    if (argc != 4) {
+        std::cout << argv[0] << " path_pca idim odim" << std::endl;
         exit(-1);
     }
     fs::path path_pca = argv[1];
+    unsigned idim = (unsigned) atoi(argv[2]);
+    unsigned odim = (unsigned) atoi(argv[3]);
     faiss::VectorTransform* pca = faiss::read_VectorTransform(path_pca.string().c_str());
+    float* aux = NULL;
+    load_data(path_pca.string().c_str(), aux, idim);
 }
