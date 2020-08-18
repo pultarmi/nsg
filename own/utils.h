5a9de69ab9a5661007a5331f0bb7423456eacf95
pultarmi <milan.pultar@gmail.com>
2020-08-18 16:00:10 +0200

    fast push

--- own/utils.h
+++ own/utils.h
@@ -19,5 +19,6 @@ unsigned load_data(const char* filename, T*& data, unsigned query_dim);
 template <typename Iter, typename Compare>
 std::vector<int> argsort(Iter begin, Iter end, Compare comp);
 
+void dummy(int a);
 
 #endif //EFANNA2E_UTILS_H
