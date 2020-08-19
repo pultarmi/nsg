fd7dc82ffaa187cda27132f7aad8ef456cd348e5
pultarmi <milan.pultar@gmail.com>
2020-08-19 10:19:25 +0200

    fast push

--- own/run_pca.cpp
+++ own/run_pca.cpp
@@ -16,21 +16,21 @@
 namespace fs = std::experimental::filesystem;
 
 
-// ~/nsg/build/own/run_pca /home/mpultar/Data/pca-P5-36 512 128
+// ~/nsg/build/own/run_pca /home/mpultar/Data/pca-P5-36 /home/mpultar/Providers/P2/embeds0.fvecs 512 128
 int main(int argc, char **argv) {
-    if (argc != 4) {
-        std::cout << argv[0] << " path_pca idim odim" << std::endl;
+    if (argc != 5) {
+        std::cout << argv[0] << " path_pca path_data idim odim" << std::endl;
         exit(-1);
     }
     fs::path path_pca = argv[1];
-    unsigned idim = (unsigned) atoi(argv[2]);
-    unsigned odim = (unsigned) atoi(argv[3]);
+    fs::path path_data = argv[2];
+    unsigned idim = (unsigned) atoi(argv[3]);
+    unsigned odim = (unsigned) atoi(argv[4]);
     faiss::VectorTransform* pca = faiss::read_VectorTransform(path_pca.string().c_str());
 //    float* aux = NULL;
 //    faiss::idx_t n;
-    auto path_data = "/home/mpultar/Providers/P2/embeds0.fvecs";
     float* data = NULL;
-    load_data(path_data, data, idim);
+    load_data(path_data.string().c_str(), data, idim);
     float* aux = pca->apply(1, data);
     for(unsigned i=0; i<50;i++){
         std::cout << aux[i] << std::endl;
