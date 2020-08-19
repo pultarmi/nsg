bef7aee42b99d1f80decb011960a81fffa0f7775
pultarmi <milan.pultar@gmail.com>
2020-08-19 12:27:57 +0200

    fast push

--- own/run_pca.cpp
+++ own/run_pca.cpp
@@ -16,12 +16,13 @@
 namespace fs = std::experimental::filesystem;
 
 
-//std::vector<float> transform(faiss::VectorTransform* pca, float* embeds, unsigned num_embeds){
-////    std::unique_ptr<float[]> aux(pca->apply(num_embeds, embeds));
-//    auto aux = pca->apply(num_embeds, embeds);
-//    std::vector<float> out(std::begin(aux), std::end(aux));
-//    return aux;
-//}
+
+float* transform(faiss::VectorTransform* pca, unsigned num_embeds, float* embeds){
+//    std::unique_ptr<float[]> aux(pca->apply(num_embeds, embeds));
+    auto aux = pca->apply(num_embeds, embeds);
+    std::vector<float> out(std::begin(aux), std::end(aux));
+    return aux;
+}
 
 //vector<float> combine(std::vector<float*> embeds){
 //}
@@ -31,27 +32,30 @@ std::unique_ptr<faiss::VectorTransform> get_pca(const fs::path* path_pca){
     return aux;
 }
 
+class Input_data{
+public:
+    fs::path path_pca;
+    fs::path path_data;
+    unsigned idim;
+    unsigned odim;
+    Input_data(int argc, char **argv){
+        if (argc != 5) {
+            std::cout << argv[0] << " path_pca path_data idim odim" << std::endl;
+            exit(-1);
+        }
+        path_pca = argv[1];
+        path_data = argv[2];
+        idim = (unsigned) atoi(argv[3]);
+        odim = (unsigned) atoi(argv[4]);
+    }
+};
+
 // ~/nsg/build/own/run_pca /home/mpultar/Data/pca-P5-36 /home/mpultar/Providers/P2/embeds0.fvecs 512 128
 int main(int argc, char **argv) {
-    if (argc != 5) {
-        std::cout << argv[0] << " path_pca path_data idim odim" << std::endl;
-        exit(-1);
-    }
-    fs::path path_pca = argv[1];
-    fs::path path_data = argv[2];
-    unsigned idim = (unsigned) atoi(argv[3]);
-    unsigned odim = (unsigned) atoi(argv[4]);
-//    faiss::VectorTransform* pca = faiss::read_VectorTransform(path_pca.string().c_str());
-    auto pca = get_pca(&path_pca);
+    Input_data I(argc, argv);
+    auto pca = get_pca(&I.path_pca);
     float* data = NULL;
-    load_data(path_data.string().c_str(), data, idim);
+    load_data(I.path_data.string().c_str(), data, I.idim);
     float* aux = pca->apply(2, data);
-    for(unsigned i=0; i<128;i++){
-        std::cout << aux[i] << std::endl;
-    }
-    std::cout << "----" << std::endl;
-    for(unsigned i=0; i<128;i++){
-        std::cout << aux[i+128] << std::endl;
-    }
 //    load_data(path_pca.string().c_str(), aux, idim);
 }
